a216bcc1943a60b1197239b0a3508d89fdb49360
Sebastian Johnsson <sebastian@agiley.se>
2012-09-25 14:39:18 +0200

    Fixing minor typo

--- resources/user.rb
+++ resources/user.rb
@@ -2,7 +2,7 @@ actions :add, :remove
 
 attribute :username,   :kind_of => String,  :name_attribute => true
 attribute :password,   :kind_of => String,  :required => true
-attribute :create_dir, :kind_of => Boolean, :default => true
+attribute :create_dir, :kind_of => String,  :default => "true"
 attribute :root,       :kind_of => String
 attribute :local_user, :kind_of => String
 
