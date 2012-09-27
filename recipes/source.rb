fc6fd06bd37ddc9af19bd4021eb3c1f5acaa00a2
Sebastian Johnsson <sebastian@agiley.se>
2012-09-27 14:45:12 +0200

    Update source recipe

--- recipes/source.rb
+++ recipes/source.rb
@@ -3,23 +3,18 @@ include_recipe "build-essential"
 source_url        =   "https://security.appspot.com/downloads/vsftpd-#{node[:vsftpd][:version]}.tar.gz"
 src_filepath      =   "#{Chef::Config['file_cache_path'] || '/tmp'}/vsftpd-#{node[:vsftpd][:version]}.tar.gz"
 
+packages          =   ['libssl-dev', 'libpam0g-dev', 'libcap2']
+packages.each do |dev_pkg|
+  package dev_pkg
+end
+
 remote_file source_url do
   source source_url
   path src_filepath
   backup false
 end
 
-packages      =   ['libssl-dev', 'libpam0g-dev', 'libcap2']
-
 node.set[:vsftpd][:binary_path] = "/usr/sbin/vsftpd"
-
-packages.each do |dev_pkg|
-  package dev_pkg
-end
-
-configure_flags   =   (node[:vsfpd][:source][:configure_flags] && node[:vsfpd][:source][:configure_flags].any?) ? node[:vsfpd][:source][:configure_flags].join(" ") : ""
-user              =   node[:vsfpd][:source][:user]      ?   node[:vsfpd][:source][:user]   :   "root"
-group             =   node[:vsfpd][:source][:user]      ?   node[:vsfpd][:source][:group]  :   "root"
 
 bash "compile_vsftpd_source" do
   cwd ::File.dirname(src_filepath)
