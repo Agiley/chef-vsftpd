60535ebe5418bc43dc338d7d885151d2713b37d2
Sebastian Johnsson <sebastian@agiley.se>
2012-09-27 15:18:17 +0200

    Create folder before uploading config

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,17 +5,17 @@ service "vsftpd" do
   action :enable
 end
 
+directory "/etc/vsftpd" do
+  owner "root"
+  group "root"
+  mode 0755
+end
+
 template node[:vsftpd][:config_path] do
   owner "root"
   group "root"
   mode 0644
   notifies :restart, resources(:service => "vsftpd"), :delayed
-end
-
-directory "/etc/vsftpd" do
-  owner "root"
-  group "root"
-  mode 0755
 end
 
 if node['vsftpd']['chroot_local_user'] or node['vsftpd']['chroot_list_enable']
