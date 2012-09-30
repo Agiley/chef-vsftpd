88813d100fd80c22c9bceae9c96640da735305c6
Sebastian Johnsson <sebastian@agiley.se>
2012-09-30 18:47:16 +0200

    Mysql fixes

--- providers/user_mysql.rb
+++ providers/user_mysql.rb
@@ -2,13 +2,16 @@ action :add do
   
   Chef::Log.info "Adding new user_mysql-resource: #{new_resource.inspect}"
   
+  username = new_resource.username
+  password = new_resource.password
+  
   execute_sql_file do
     template_cookbook "vsftpd"
     template_path     '/tmp/add_vsftpd_user.sql'
     template_source   'mysql/add_user.sql.erb'
     
-    variables :user       =>  new_resource.username,
-              :password   =>  new_resource.password
+    variables :user       =>  username,
+              :password   =>  password
   end
 
   if (node[:vsftpd][:create_user_configs])
@@ -25,13 +28,16 @@ action :add do
 end
 
 action :remove do
+  username = new_resource.username
+  password = new_resource.password
+  
   execute_sql_file do
     template_cookbook "vsftpd"
     template_path     '/tmp/remove_vsftpd_user.sql'
     template_source   'mysql/remove_user.sql.erb'
     
-    variables :user       =>  new_resource.username,
-              :password   =>  new_resource.password
+    variables :user       =>  username,
+              :password   =>  password
   end
 
   file "#{node[:vsftpd][:user_config_dir]}/#{new_resource.username}" do
